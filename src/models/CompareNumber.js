adaab4c13951a4c7bae1303f77ca7ce1d06e9e07
2smallears <2smallears@users.noreply.github.com>
2016-07-26 14:38:02 +0800

    refactor

--- src/models/CompareNumber.js
+++ src/models/CompareNumber.js
@@ -2,26 +2,23 @@ class CompareNumber{
 
     static getCompareResult(input, answer){
 
-        if (input === answer){
-            return '4A0B';
-        } else {
-            const inputArr = input.split('');
-            const answerArr = answer.split('');
+        const inputArr = input.split('');
+        const answerArr = answer.split('');
 
-            const sameNum = answerArr.filter(num => inputArr.find(input => input === num));
+        const sameNum = answerArr.filter(num => inputArr.find(input => input === num));
 
-            const resultArr = inputArr.map(i => {
-                if (inputArr.indexOf(i) === answerArr.indexOf(i)){
-                    return 'A';
-                } else {
-                    return 'B';
-                }
-            });
+        const resultArr = inputArr.map(i => {
+            if (inputArr.indexOf(i) === answerArr.indexOf(i)){
+                return 'A';
+            } else {
+                return 'B';
+            }
+        });
 
-            const countA = resultArr.filter(i => i === 'A');
+        const countA = resultArr.filter(i => i === 'A');
+        const countB = sameNum.length - countA.length;
 
-            return `${countA.length}A${sameNum.length - countA.length}B`;
-        }
+        return `${countA.length}A${countB}B`;
     }
 }
 
